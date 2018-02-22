cf4e83496592e2f9e9acc039dd6bb4281fdae64d
piyushsoni27 <piy1995s@gmail.com>
2018-02-23 00:42:49 +0530

    Added XGBoost model

--- Loan Prediction/ML.py
+++ Loan Prediction/ML.py
@@ -9,40 +9,42 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 
+from xgboost import XGBClassifier
+
 
 def classification_model(model, data, predictors, outcome):
-    
+
     model.fit(data[predictors], data[outcome])
-    
+
     predictions = model.predict(data[predictors])
-    
-    accuracy = metrics.accuracy_score(predictions,data[outcome])
+
+    accuracy = metrics.accuracy_score(predictions, data[outcome])
     print("Accuracy : %s" % "{0:.3%}".format(accuracy))
-    
+
     kf = KFold(data.shape[0], n_folds=5)
-    error=[]
-    
+    error = []
+
     for train, test in kf:
-        train_predictors = (data[predictors].iloc[train,:])
-        
+        train_predictors = (data[predictors].iloc[train, :])
+
         train_target = data[outcome].iloc[train]
-        
+
         model.fit(train_predictors, train_target)
-        
-        error.append(model.score(data[predictors].iloc[test,:], data[outcome].iloc[test]))
-        
-        
+
+        error.append(model.score(data[predictors].iloc[test, :], data[outcome].iloc[test]))
+
+
     print("Cross-Validation Score : %s" % "{0: .3%}".format(np.mean(error)))
-    
+
     #Fit the model again so that it can be referred outside the function:
     model.fit(data[predictors], data[outcome])
-   
 
-train = pd.read_csv("D:\\Timepass\Loan Predictor\Data\\train_final.csv")
-test = pd.read_csv("D:\\Timepass\Loan Predictor\Data\\test_final.csv")
 
-train.drop(["Unnamed: 0","index"], axis=1, inplace=True)
-test.drop(["Unnamed: 0","index"], axis=1, inplace=True)
+train = pd.read_csv("/media/piyush/New Volume/Data Science/Loan Prediction/data//train_final.csv")
+test = pd.read_csv("/media/piyush/New Volume/Data Science/Loan Prediction/data//test.csv")
+
+# train.drop(["Unnamed: 0","index"], axis=1, inplace=True)
+# test.drop(["Unnamed: 0","index"], axis=1, inplace=True)
 
 ## Convert categorical to numeric variables
 ## LabelEncoder --> Ordinal variables
@@ -53,7 +55,7 @@ le = LabelEncoder()
 
 for i in cat_var:
     train[i] = le.fit_transform(train[i])
-    
+
 
 ## Logistic Regression
 print("Logistic Regression:")
@@ -67,7 +69,7 @@ predictor_var = ['Credit_History','Education','Married','Self_Employed','Propert
 classification_model(model, train, predictor_var, outcome_var)
 
 ## Decision Tree
-print("\nDecision Tree:") 
+print("\nDecision Tree:")
 model = DecisionTreeClassifier()
 predictor_var = ['Credit_History','Gender','Married','Education']
 classification_model(model, train, predictor_var,outcome_var)
@@ -96,19 +98,23 @@ Thus we calculate feature imp and only use top 5 features
 featimp = pd.Series(model.feature_importances_, index=predictor_var).sort_values(ascending=False)
 
 print("\nFeature Importances:\n",featimp)
-    
-    
+
+
 ## Using top 5 imp features
-print("\n Random Forest with top 5 imp features")
+print("\nRandom Forest with top 5 imp features")
 model = RandomForestClassifier(n_estimators=25, min_samples_split=25, max_depth=7, max_features=1)
 predictor_var = ['Credit_History', 'total_income', 'LoanAmount', 'Dependents', 'Property_Area']
 classification_model(model, train, predictor_var, outcome_var)
 
 
-    
-    
-    
-    
-    
-    
-    
+## XGBoost Classifier
+print("\nXGBoost Classifier:")
+
+model = XGBClassifier()
+predictor_var = ['Credit_History', 'Dependents', 'Education', 'Gender', 'LoanAmount',
+       'Loan_Amount_Term', 'Married', 'Property_Area',
+       'Self_Employed', 'total_income']
+classification_model(model, train, predictor_var, outcome_var)
+
+
+
