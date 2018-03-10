29254542d1fbc97febbf8e44d77d7f02a53aeb8d
piyushsoni27 <piy1995s@gmail.com>
2018-03-10 12:45:32 +0530

    Updated ml_models.py

--- Analytics Vidhya - Big Mart Sales III/ml_models.py
+++ Analytics Vidhya - Big Mart Sales III/ml_models.py
@@ -7,45 +7,106 @@ Created on Sat Jan 27 00:00:06 2018
 
 import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+from math import sqrt
+
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.metrics import mean_squared_error
+from sklearn.cross_validation import KFold
+
 from matplotlib import pyplot as plt
 
-def model_fit(alg, xtrain, ytrain, cv_ratio):
-    # create training and testing vars
-    X_train, X_test, Y_train, Y_test = train_test_split(xtrain, ytrain, test_size=cv_ratio)
+test_original = pd.read_csv("D:\\Timepass\Big mart\Data\\test.csv")
+
+def regression_model(alg, data, predictors, target, n_fold=5, save=False, test_df=None, file_path=None):
     
-    alg.fit(X_train, Y_train)
+    alg.fit(data[predictors], data[target])
     
-    train_prediction = alg.predict(X_train)
-    test_prediction = alg.predict(X_test)
+    predictions = alg.predict(data[predictors])
     
-    ## Mean Squared Error train set
-    print("Mean Squared Error Train: %.2f",  mean_squared_error(Y_train, train_prediction))
+    error = sqrt(mean_squared_error(data[target], predictions))
+    print("Train Error : %s" % "{0:}".format(error))
     
-    ## Mean Squared Error test set
-    print("Mean Squared Error Test: %.2f",  mean_squared_error(Y_test, test_prediction))
+    kf = KFold(data.shape[0], n_folds=n_fold)
+    fold_error = []
     
-    ## Variance Error train:
-    print("Variance Error Train: %.2f",  r2_score(Y_train, train_prediction))
+    for train, test in kf:
+        train_predictors = data[predictors].iloc[train, :]
+        train_target = data[target].iloc[train]
+        
+        alg.fit(train_predictors, train_target)
+        
+        fold_error.append(sqrt(mean_squared_error(data[target].iloc[test], alg.predict(data[predictors].iloc[test, :]))))
+        
+    print("Cross-Validation Score : %s" % "{0:}".format(np.mean(fold_error)))
     
-    ## Variance Error test:
-    print("Variance Error Test: %.2f",  r2_score(Y_test, test_prediction))
+    alg.fit(data[predictors], data[target])
     
-    return
+    if(save):
+        test_predictions = alg.predict(test_df[predictors])
+        
+        base2 = test_original[['Item_Identifier', 'Outlet_Identifier']]
+        base2["Item_Outlet_Sales"] = test_predictions
+    
+        base2.to_csv(file_path, index=False)
+    
+    
+    
+train = pd.read_csv("D:\\Timepass\Big mart\Data\\train_final.csv")
+test = pd.read_csv("D:\\Timepass\Big mart\Data\\test_final.csv")
+test_original = pd.read_csv("D:\\Timepass\Big mart\Data\\test.csv")
+
+test.drop("Unnamed: 0", inplace=True, axis=1)
+
+predictors = ['Item_Fat_Content', 'Item_MRP', 'Item_Type',
+       'Item_Visibility', 'Item_Weight', 'Outlet_Identifier',
+       'Outlet_Location_Type', 'Outlet_Size', 'Outlet_Type',
+       'Outlet_Establishment_Year_from_2018', 'Item_Code', 'Item_Category']
+
+target = 'Item_Outlet_Sales'
+
+## Baseline (LeaderBoard : 1773)
+mean_sales = train.Item_Outlet_Sales.mean()
+
+base1 = test_original[['Item_Identifier', 'Outlet_Identifier']]
+base1["Item_Outlet_Sales"] = mean_sales
+
+base1.to_csv("D:\Timepass\Big mart\Submissions\mean.csv", index=False)
+
+## Linear Regression (LeaderBoard : 1232)
+print("\nLinear :\n")
+lm = LinearRegression()
+regression_model(lm, train, predictors, target)
 
 
-# fit a model Linear Regression
-lm = linear_model.LinearRegression()
-
-model = lm.fit(X_train, Y_train)
-predictions = lm.predict(X_test)
-print(model.score)
-
-## Ridge model
-predictors = [x for x in X_train.columns if x not in [target]+IDcol]
-alg2 = Ridge(alpha=0.05,normalize=True)
-modelfit(alg2, train, test, predictors, target, IDcol, 'alg2.csv')
-coef2 = pd.Series(alg2.coef_, predictors).sort_values()
+## Ridge model (LeaderBoard : 1251)
+print("\nRidge :\n")
+alg = Ridge(alpha=0.05,normalize=True)
+regression_model(alg, train, predictors, target, save=False, test_df=test, file_path="D:\Timepass\Big mart\Submissions\RidgeRegression.csv")
+coef2 = pd.Series(alg.coef_, predictors).sort_values()
 coef2.plot(kind='bar', title='Model Coefficients')
+
+## Decision Trees  (LeaderBoard : 1191)
+print("\nDecision Trees :\n")
+print("alg1:")
+alg = DecisionTreeRegressor(max_depth=15, min_samples_leaf=100)
+regression_model(alg, train, predictors, target, save=False, test_df=test, file_path="D:\Timepass\Big mart\Submissions\DecisionTree.csv")
+
+print("\nalg2:") ## (LeaderBoard : 1184)
+alg = DecisionTreeRegressor(max_depth=8, min_samples_leaf=150)
+regression_model(alg, train, predictors, target, save=False, test_df=test, file_path="D:\Timepass\Big mart\Submissions\DecisionTree2.csv")
+
+## Random Forest (LeaderBoard : 1186)
+print("\nRandom Forest :\n")
+print("alg1:")
+alg = RandomForestRegressor(n_estimators=200,max_depth=5, min_samples_leaf=100,n_jobs=4)
+regression_model(alg, train, predictors, target, save=False, test_df=test, file_path="D:\Timepass\Big mart\Submissions\RandomForest.csv")
+
+print("\nalg2:") ## (LeaderBoard : 1184.79)
+alg = RandomForestRegressor(n_estimators=400,max_depth=6, min_samples_leaf=100,n_jobs=4)
+regression_model(alg, train, predictors, target, save=True, test_df=test, file_path="D:\Timepass\Big mart\Submissions\RandomForest1.csv")
+
+
+
