b5ad13e37799e77355ce1ba1e3af282aa0ff9930
Piyush Soni <piy1995s@gmail.com>
2018-04-11 15:43:43 +0530

    Added fbprophet modelling for time series analysis

--- TimeSeries Aanalysis Stocks/main.py
+++ TimeSeries Aanalysis Stocks/main.py
@@ -1,28 +1,128 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Tue Apr 10 19:24:59 2018
 
-This is a temporary script file.
+@author: p.soni
 """
 
+# quandl for financial data
 import quandl
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Retrieve TSLA data from Quandl
-tesla = quandl.get('WIKI/TSLA')
+"""
+Prophet is designed for analyzing time series with daily observations that 
+display patterns on different time scales. It also has advanced capabilities 
+for modeling the effects of holidays on a time-series and implementing custom 
+changepoints, but we will stick to the basic functions to get a model up and 
+running.
+"""
+import fbprophet
 
-# Retrieve the GM data from Quandl
-gm = quandl.get('WIKI/GM')
+#quandl_api_key = "ybWVyfzv1-pdoTxaHs_u"
+
+#quandl.ApiConfig.api_key = quandl_api_key
+
+tesla = quandl.get("WIKI/TSLA")
+
+gm = quandl.get("WIKI/GM")
 
 # The adjusted close accounts for stock splits, so that is what we should graph
 plt.plot(gm.index, gm['Adj. Close'])
 plt.title('GM Stock Price')
 plt.ylabel('Price ($)');
 plt.show()
-
 plt.plot(tesla.index, tesla['Adj. Close'], 'r')
 plt.title('Tesla Stock Price')
 plt.ylabel('Price ($)');
 plt.show();
+
+# Yearly average number of shares outstanding for Tesla and GM
+tesla_shares = {2018: 168e6, 2017: 162e6, 2016: 144e6, 2015: 128e6, 2014: 125e6,
+                2013: 119e6, 2012: 107e6, 2011: 100e6, 2010: 51e6}
+gm_shares = {2018: 1.42e9, 2017: 1.50e9, 2016: 1.54e9, 2015: 1.59e9, 2014: 1.61e9,
+             2013: 1.39e9, 2012: 1.57e9, 2011: 1.54e9, 2010:1.50e9}
+# Create a year column 
+tesla['Year'] = tesla.index.year
+# Take Dates from index and move to Date column 
+tesla.reset_index(level=0, inplace = True)
+tesla['tesla_cap'] = 0
+
+
+# Calculate market capitalization(company value) = no. of shares * share price for all years
+for i, year in enumerate(tesla['Year']):
+    # Retrieve the shares for the year
+    shares = tesla_shares.get(year)
+    
+    # Update the cap column to shares times the price
+    tesla.loc[i, 'tesla_cap'] = shares * tesla.loc[i, 'Adj. Close']
+    
+# Create a year column 
+gm['Year'] = gm.index.year
+# Take Dates from index and move to Date column 
+gm.reset_index(level=0, inplace = True)
+gm['gm_cap'] = 0
+
+for i, year in enumerate(gm['Year']):
+    # Retrieve the shares for the year
+    shares = gm_shares.get(year)
+    
+    # Update the cap column to shares times the price
+    gm.loc[i, 'gm_cap'] = shares * gm.loc[i, 'Adj. Close']
+    
+# Merge the two datasets and rename the columns
+cars = gm.merge(tesla, how='inner', on='Date')
+
+# Select only the relevant columns
+cars = cars.loc[:, ['Date', 'gm_cap', 'tesla_cap']]
+# Divide to get market cap in billions of dollars
+cars['gm_cap'] = cars['gm_cap'] / 1e9
+cars['tesla_cap'] = cars['tesla_cap'] / 1e9
+
+plt.figure(figsize=(10, 8))
+plt.plot(cars['Date'], cars['gm_cap'], 'b-', label = 'GM')
+plt.plot(cars['Date'], cars['tesla_cap'], 'r-', label = 'TESLA')
+plt.xlabel('Date'); plt.ylabel('Market Cap (Billions $)'); plt.title('Market Cap of GM and Tesla')
+plt.legend();
+
+# Find the first and last time Tesla was valued higher than GM
+first_date = cars.loc[np.min(list(np.where(cars['tesla_cap'] > cars['gm_cap'])[0])), 'Date']
+last_date = cars.loc[np.max(list(np.where(cars['tesla_cap'] > cars['gm_cap'])[0])), 'Date']
+print("Tesla was valued higher than GM from {} to {}.".format(first_date.date(), last_date.date()))
+
+
+# Prophet requires columns ds (Date) and y (value)
+gm = gm.rename(columns={'Date': 'ds', 'gm_cap': 'y'})
+# Put market cap in billions
+gm['y'] = gm['y'] / 1e9
+# Make the prophet model and fit on the data
+gm_prophet = fbprophet.Prophet(changepoint_prior_scale=0.15)
+gm_prophet.fit(gm)
+
+
+# Make a future dataframe for 2 years
+gm_forecast = gm_prophet.make_future_dataframe(periods=365 * 2, freq='D')
+# Make predictions
+gm_forecast = gm_prophet.predict(gm_forecast)
+
+gm_prophet.plot(gm_forecast, xlabel = 'Date', ylabel = 'Market Cap (billions $)')
+plt.title('Market Cap of GM');
+
+# Prophet requires columns ds (Date) and y (value)
+tesla = tesla.rename(columns={'Date': 'ds', 'tesla_cap': 'y'})
+# Put market cap in billions
+tesla['y'] = tesla['y'] / 1e9
+# Make the prophet model and fit on the data
+tesla_prophet = fbprophet.Prophet(changepoint_prior_scale=0.15)
+tesla_prophet.fit(tesla)
+
+
+# Make a future dataframe for 2 years
+tesla_forecast = tesla_prophet.make_future_dataframe(periods=365 * 2, freq='D')
+# Make predictions
+tesla_forecast = tesla_prophet.predict(gm_forecast)
+
+tesla_prophet.plot(tesla_forecast, xlabel = 'Date', ylabel = 'Market Cap (billions $)')
+plt.title('Market Cap of Tesla');
