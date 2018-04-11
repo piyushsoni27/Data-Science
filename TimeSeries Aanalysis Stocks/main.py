43dbe81464bbc2d0155138184d9a63e8cca089c1
piyushsoni27 <piy1995s@gmail.com>
2018-04-12 01:46:19 +0530

    added more visualisations

--- TimeSeries Aanalysis Stocks/main.py
+++ TimeSeries Aanalysis Stocks/main.py
@@ -114,7 +114,16 @@ plt.title('Market Cap of GM');
 tesla = tesla.rename(columns={'Date': 'ds', 'tesla_cap': 'y'})
 # Put market cap in billions
 tesla['y'] = tesla['y'] / 1e9
+
 # Make the prophet model and fit on the data
+"""
+changepoint_prior_scale: his hyperparameter is used to control how sensitive 
+the trend is to changes, with a higher value being more sensitive and a lower
+ value less sensitive. This value is used to combat one of the most fundamental
+ trade-offs in machine learning: bias vs. variance.
+ 
+ Default value = 0.05
+"""
 tesla_prophet = fbprophet.Prophet(changepoint_prior_scale=0.15)
 tesla_prophet.fit(tesla)
 
@@ -126,3 +135,76 @@ tesla_forecast = tesla_prophet.predict(gm_forecast)
 
 tesla_prophet.plot(tesla_forecast, xlabel = 'Date', ylabel = 'Market Cap (billions $)')
 plt.title('Market Cap of Tesla');
+
+
+# Try 4 different changepoints
+for changepoint in [0.001, 0.05, 0.1, 0.5]:
+    model = fbprophet.Prophet(daily_seasonality=False, changepoint_prior_scale=changepoint)
+    model.fit(tesla)
+    
+    future = model.make_future_dataframe(periods=365, freq='D')
+    future = model.predict(future)
+    
+    tesla[changepoint] = future['yhat']
+
+# Create the plot
+plt.figure(figsize=(10, 8))
+
+# Actual observations
+plt.plot(tesla['ds'], tesla['y'], 'ko', label = 'Observations')
+colors = {0.001: 'b', 0.05: 'r', 0.1: 'grey', 0.5: 'gold'}
+
+# Plot each of the changepoint predictions
+for changepoint in [0.001, 0.05, 0.1, 0.5]:
+    plt.plot(tesla['ds'], tesla[changepoint], color = colors[changepoint], label = '%.3f prior scale' % changepoint)
+    
+plt.legend(prop={'size': 14})
+plt.xlabel('Date'); plt.ylabel('Market Cap (billions $)'); plt.title('Effect of Changepoint Prior Scale');
+
+gm_names = ['gm_%s' % column for column in gm_forecast.columns]
+tesla_names = ['tesla_%s' % column for column in tesla_forecast.columns]
+
+# Dataframes to merge
+merge_gm_forecast = gm_forecast.copy()
+merge_tesla_forecast = tesla_forecast.copy()
+
+# Rename the columns
+merge_gm_forecast.columns = gm_names
+merge_tesla_forecast.columns = tesla_names
+
+# Merge the two datasets
+forecast = pd.merge(merge_gm_forecast, merge_tesla_forecast, how = 'inner', left_on = 'gm_ds', right_on = 'tesla_ds')
+
+# Rename date column
+forecast = forecast.rename(columns={'gm_ds': 'Date'}).drop('tesla_ds', axis=1)
+
+plt.figure(figsize=(10, 8))
+plt.plot(forecast['Date'], forecast['gm_trend'], 'b-')
+plt.plot(forecast['Date'], forecast['tesla_trend'], 'r-')
+plt.legend(); plt.xlabel('Date'); plt.ylabel('Market Cap ($)')
+plt.title('GM vs. Tesla Trend');
+
+plt.figure(figsize=(10, 8))
+plt.plot(forecast['Date'], forecast['gm_yhat'], 'b-')
+plt.plot(forecast['Date'], forecast['tesla_yhat'], 'r-')
+plt.legend(); plt.xlabel('Date'); plt.ylabel('Market Cap (billions $)')
+plt.title('GM vs. Tesla Estimate');
+
+
+overtake_date = min(forecast.loc[forecast['tesla_yhat'] > forecast['gm_yhat'], 'Date'])
+print('Tesla overtakes GM on {}'.format(overtake_date))
+
+# Create subplots to set figure size
+fig, ax = plt.subplots(1, 1, figsize=(10, 8));
+
+# Plot estimate
+ax.plot(forecast['Date'], forecast['gm_yhat'], label = 'gm prediction');
+
+# Plot uncertainty values
+ax.fill_between(forecast['Date'].dt.to_pydatetime(), forecast['gm_yhat_upper'], forecast['gm_yhat_lower'], alpha=0.6, edgecolor = 'k');
+
+# Plot estimate and uncertainty for tesla
+ax.plot(forecast['Date'], forecast['tesla_yhat'], 'r', label = 'tesla prediction');
+ax.fill_between(forecast['Date'].dt.to_pydatetime(), forecast['tesla_yhat_upper'], forecast['tesla_yhat_lower'], alpha=0.6, edgecolor = 'k');
+plt.legend();
+plt.xlabel('Date'); plt.ylabel('Billions $'); plt.title('Market Cap Prediction for GM and Tesla');
