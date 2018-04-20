6db37396c077ae6a6e898e5b6d50bb39e8a4350e
Piyush Soni <piy1995s@gmail.com>
2018-04-20 18:05:26 +0530

    Update ARIMA_model.py

--- Time Series Analysi_ Air passenger forecast/ARIMA_model.py
+++ Time Series Analysi_ Air passenger forecast/ARIMA_model.py
@@ -4,10 +4,53 @@ Created on Wed Apr 18 20:31:17 2018
 
 @author: p.soni
 """
+
+import numpy as np
 import pandas as pd
+import matplotlib.pylab as plt
+
 #ACF and PACF plots:
 from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA
 
-ts_log_diff = pd.read_csv("ts_log_diff.csv", header=None, names = ["Month", '#Passengers' ], index_col=0, squeeze=True)
+ts_log = pd.read_csv("ts_log.csv", header=None, names = ["Month", '#Passengers' ], index_col=0, squeeze=True)
                                                                    
-ts_log_diff.index=pd.to_datetime(ts_log_diff.index, format='%Y-%m')
+ts_log.index=pd.to_datetime(ts_log.index, format='%Y-%m')
+
+ts_log_diff = ts_log - ts_log.shift() 
+ts_log_diff.dropna(inplace=True)
+
+lag_acf = acf(ts_log_diff, nlags=20)
+lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+                                                                   
+#Plot ACF: 
+plt.subplot(121) 
+plt.plot(lag_acf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Autocorrelation Function')
+
+#Plot PACF:
+plt.subplot(122)
+plt.plot(lag_pacf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Partial Autocorrelation Function')
+plt.tight_layout()
+
+"""
+In this plot, the two dotted lines on either sides of 0 are the confidence interevals. These can be used to determine the ‘p’ and ‘q’ values as:
+
+p – The lag value where the PACF chart crosses the upper confidence interval for the first time. If you notice closely, in this case p=2.
+q – The lag value where the ACF chart crosses the upper confidence interval for the first time. If you notice closely, in this case q=2.
+
+"""
+
+# AR model
+model = ARIMA(ts_log, order=(2, 1, 0))  
+results_AR = model.fit(disp=-1)  
+plt.plot(ts_log_diff)
+plt.plot(results_AR.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_AR.fittedvalues-ts_log_diff)**2))
